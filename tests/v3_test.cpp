78f978ea66146112b6122b74e6957fffd7bfe8f7
Chesium <chesium@hotmail.com>
2025-12-21 22:39:14 +0800

    feat: 4.1 ray class

--- tests/v3_test.cpp
+++ tests/v3_test.cpp
@@ -2,14 +2,14 @@
 
 #include "v3.hpp"
 
-TEST(V3Test, DefaultConstructsToZero) {
+TEST(LibTest, DefaultConstructsToZero) {
   v3 v;
   EXPECT_DOUBLE_EQ(v.x(), 0.0);
   EXPECT_DOUBLE_EQ(v.y(), 0.0);
   EXPECT_DOUBLE_EQ(v.z(), 0.0);
 }
 
-TEST(V3Test, AddsAndSubtractsComponentWise) {
+TEST(LibTest, AddsAndSubtractsComponentWise) {
   v3 a(1.0, 2.0, 3.0);
   v3 b(4.0, 5.0, 6.0);
 
@@ -25,7 +25,7 @@ TEST(V3Test, AddsAndSubtractsComponentWise) {
   EXPECT_DOUBLE_EQ(diff.z(), 3.0);
 }
 
-TEST(V3Test, DotAndCrossProducts) {
+TEST(LibTest, DotAndCrossProducts) {
   v3 x_axis(1.0, 0.0, 0.0);
   v3 y_axis(0.0, 1.0, 0.0);
   v3 z_axis(0.0, 0.0, 1.0);
@@ -39,9 +39,23 @@ TEST(V3Test, DotAndCrossProducts) {
   EXPECT_DOUBLE_EQ(cross_xy.z(), z_axis.z());
 }
 
-TEST(V3Test, UnitVectorHasLengthOne) {
+TEST(LibTest, UnitVectorHasLengthOne) {
   v3 v(0.0, 3.0, 4.0);
   v3 u = unit(v);
 
   EXPECT_NEAR(u.l(), 1.0, 1e-12);
+}
+
+#include "ray.hpp"
+
+TEST(LibTest, RayAtMethod) {
+  point3 o(1.0, 2.0, 3.0);
+  v3 d(-2.0, 3.0, 0.5);
+  ray r(o, d);
+  double t = 2.0;
+  point3 t2 = r.at(t);
+  point3 ref(-3.0, 8.0, 4.0);
+  point3 err = t2 - ref;
+
+  EXPECT_NEAR(err.l(), 0.0, 1e-12);
 }
